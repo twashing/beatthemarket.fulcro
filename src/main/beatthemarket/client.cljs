3a646f29b46b3b100e8835c2b00011f312c421e6
Timothy Washington <twashing@interruptsoftware.com>
2020-04-23 20:43:24 -0400

    Have working Fulcro Root :initial-state   :query and :ident on child components

--- src/main/beatthemarket/client.cljs
+++ src/main/beatthemarket/client.cljs
@@ -5,6 +5,7 @@
             [fulcro.client.network :as net]
             [fulcro.client.data-fetch :as df]
 
+            [com.fulcrologic.fulcro.routing.dynamic-routing :as dr]
             [com.fulcrologic.fulcro.networking.websockets :as fws]
             [com.fulcrologic.fulcro.application :as app]
 
@@ -16,11 +17,11 @@
 
 
 (defonce ^:export app (atom nil))
-(def secured-request-middleware
-  ;; The CSRF token is embedded via server_components/html.clj
-  (->
-    (net/wrap-csrf-token (or js/fulcro_network_csrf_token "TOKEN-NOT-IN-HTML!"))
-    (net/wrap-fulcro-request)))
+;; (def secured-request-middleware
+;;   ;; The CSRF token is embedded via server_components/html.clj
+;;   (->
+;;     (net/wrap-csrf-token (or js/fulcro_network_csrf_token "TOKEN-NOT-IN-HTML!"))
+;;     (net/wrap-fulcro-request)))
 
 (defmutation ^:export connect-socket [_]
   (websocket [_] true))
@@ -40,7 +41,6 @@
   (log/info "push-handler received /" data)
   (log/info "msg /" msg)
 
-  ;; Highcharts.charts[0].series[0].addPoint(eval(e.data), true, false);
   (.addPoint (get-chart-series) (msg true false)))
 
 
@@ -54,17 +54,36 @@
   (reset! app (app/fulcro-app {;; :client-did-mount (fn [beatthemarket]
                                ;;                     (df/load beatthemarket :all-users stock-chart/StockChart))
 
-                               :remotes {:remote (net/fulcro-http-remote
-                                                   {:url                "/api"
-                                                    :request-middleware secured-request-middleware})
-                                         :websocket (fws/fulcro-websocket-remote
-                                                      {:csrf-token js/fulcro_network_csrf_token
-                                                       :push-handler push-handler
-                                                       :state-callback state-callback})}}))
-  (app/mount! @app root/Root "app"))
+                               ;; :remotes {:remote (net/fulcro-http-remote
+                               ;;                     {:url                "/api"
+                               ;;                      ;; :request-middleware secured-request-middleware
+                               ;;                      })
+                               ;;           :websocket (fws/fulcro-websocket-remote
+                               ;;                        {;; :csrf-token js/fulcro_network_csrf_token
+                               ;;                         :push-handler push-handler
+                               ;;                         :state-callback state-callback})}
+                               }))
+  (app/mount! @app root/Root "app" {:initialize-state? true})
+  (dr/initialize! @app)
+  (dr/change-route app (dr/path-to root/Index)))
 
-(.addEventListener
-  js/window "DOMContentLoaded"
-  (fn []
-    (.log js/console "DOMContentLoaded callback")
-    (init)))
+
+(comment
+
+  (ns beatthemarket.client)
+  (require '[com.fulcrologic.fulcro.algorithms.merge :as merge]
+           '[com.fulcrologic.fulcro.routing.dynamic-routing :as dr]
+           '[com.fulcrologic.fulcro.application :as app]
+           '[com.fulcrologic.fulcro.components :as comp]
+           '[com.fulcrologic.fulcro.mutations :as m :refer [defmutation]])
+
+  (app/current-state app)
+  (comp/get-initial-state root/Root)
+  (comp/get-query root/Root)
+
+  (merge/merge-component!
+    app root/Root {:index/id 2
+                   :index/text "Foobar"})
+
+  (dr/change-route app (dr/path-to root/Index))
+  (dr/change-route app ["index"]))
