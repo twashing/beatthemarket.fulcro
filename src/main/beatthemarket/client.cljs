3edd9c73ccfb66e02eb5931c0e59073224e55d70
Timothy Washington <twashing@gmail.com>
2020-04-26 15:10:34 -0400

    Wire up navigation (#4)
    * Have working Fulcro Root :initial-state
      :query and :ident on child components
    
    * First pass at working dynamic routing, using the change-route function
    
    * Some code cleanup
    
    * First pass at adding HTML5 History and 2-way binding between browser bar and app state
    
    Co-authored-by: Timothy Washington <twashing@interruptsoftware.com>

--- src/main/beatthemarket/client.cljs
+++ src/main/beatthemarket/client.cljs
@@ -1,10 +1,14 @@
 (ns beatthemarket.client
-  (:require [taoensso.timbre :as log]
+  (:require [clojure.string :as str]
+            [taoensso.timbre :as log]
+            [pushy.core :as pushy]
+            [edn-query-language.core :as eql]
             [fulcro.client :as fc]
 
             [fulcro.client.network :as net]
             [fulcro.client.data-fetch :as df]
 
+            [com.fulcrologic.fulcro.routing.dynamic-routing :as dr]
             [com.fulcrologic.fulcro.networking.websockets :as fws]
             [com.fulcrologic.fulcro.application :as app]
 
@@ -16,11 +20,11 @@
 
 
 (defonce ^:export app (atom nil))
-(def secured-request-middleware
-  ;; The CSRF token is embedded via server_components/html.clj
-  (->
-    (net/wrap-csrf-token (or js/fulcro_network_csrf_token "TOKEN-NOT-IN-HTML!"))
-    (net/wrap-fulcro-request)))
+;; (def secured-request-middleware
+;;   ;; The CSRF token is embedded via server_components/html.clj
+;;   (->
+;;     (net/wrap-csrf-token (or js/fulcro_network_csrf_token "TOKEN-NOT-IN-HTML!"))
+;;     (net/wrap-fulcro-request)))
 
 (defmutation ^:export connect-socket [_]
   (websocket [_] true))
@@ -28,6 +32,47 @@
 (defn ^:export connect-websocket []
   (comp/transact! app `[(connect-socket {})]))
 
+
+;; ====>
+
+(defn url->path
+  "Given a url of the form \"/gift/123/edit?code=abcdef\", returns a
+  path vector of the form [\"gift\" \"123\" \"edit\"]. Assumes the url
+  starts with a forward slash. An empty url yields the path [\"home\"]
+  instead of []."
+  [url]
+  (-> url (str/split "?") first (str/split "/") rest vec))
+
+(defn path->url
+  "Given a path vector of the form [\"gift\" \"123\" \"edit\"],
+  returns a url of the form \"/gift/123/edit\"."
+  [path]
+  (str/join (interleave (repeat "/") path)))
+
+(defn routable-path?
+  "True if there exists a router target for the given path."
+  [app path]
+  (let [state-map  (app/current-state app)
+        root-class (app/root-class @app)
+        root-query (comp/get-query root-class state-map)
+        ast        (eql/query->ast root-query)]
+    (some? (dr/ast-node-for-route ast path))))
+
+(def default-route ["index"])
+
+(defonce history (pushy/pushy
+                   (fn [path]
+                     (log/info "pushy/pushy A /" path)
+                     (dr/change-route app path))
+                   (fn [url]
+                     (log/info "pushy/pushy B /" url)
+                     (let [path (url->path url)]
+                       (log/info "pushy/pushy B.i /" path)
+                       (if (routable-path? app path)
+                         path
+                         default-route)))))
+
+;; ====>
 
 (defn get-chart-series []
   (first
@@ -40,7 +85,6 @@
   (log/info "push-handler received /" data)
   (log/info "msg /" msg)
 
-  ;; Highcharts.charts[0].series[0].addPoint(eval(e.data), true, false);
   (.addPoint (get-chart-series) (msg true false)))
 
 
@@ -54,17 +98,61 @@
   (reset! app (app/fulcro-app {;; :client-did-mount (fn [beatthemarket]
                                ;;                     (df/load beatthemarket :all-users stock-chart/StockChart))
 
-                               :remotes {:remote (net/fulcro-http-remote
-                                                   {:url                "/api"
-                                                    :request-middleware secured-request-middleware})
-                                         :websocket (fws/fulcro-websocket-remote
-                                                      {:csrf-token js/fulcro_network_csrf_token
-                                                       :push-handler push-handler
-                                                       :state-callback state-callback})}}))
-  (app/mount! @app root/Root "app"))
+                               ;; :remotes {:remote (net/fulcro-http-remote
+                               ;;                     {:url                "/api"
+                               ;;                      ;; :request-middleware secured-request-middleware
+                               ;;                      })
+                               ;;           :websocket (fws/fulcro-websocket-remote
+                               ;;                        {;; :csrf-token js/fulcro_network_csrf_token
+                               ;;                         :push-handler push-handler
+                               ;;                         :state-callback state-callback})}
+                               }))
+  (app/mount! @app root/Root "app" {:initialize-state? true})
+  (dr/initialize! @app)
+  (pushy/start! history)
+  (dr/change-route app (dr/path-to root/Index)))
 
-(.addEventListener
-  js/window "DOMContentLoaded"
-  (fn []
-    (.log js/console "DOMContentLoaded callback")
-    (init)))
+(defn route-to! [path]
+
+  ;; (log/info "route-to! /" path)
+  (pushy/set-token! history (path->url path)))
+
+;; TODO On initial load, use URI route
+;; TODO Fix route-to mutation
+(defmutation route-to
+  "Mutation to go to a specific route"
+  [{:keys [path]}]
+  (action [_]
+          (log/info "route-to / path /" path)
+          (route-to! path)))
+
+(comment
+
+  ;; shadow-cljs clj-repl
+  (shadow/watch :main)
+  (shadow/repl :main)
+
+  (shadow/repl :main)
+
+
+  (ns beatthemarket.client)
+  (require '[com.fulcrologic.fulcro.algorithms.merge :as merge]
+           '[com.fulcrologic.fulcro.routing.dynamic-routing :as dr]
+           '[com.fulcrologic.fulcro.application :as app]
+           '[com.fulcrologic.fulcro.components :as comp]
+           '[com.fulcrologic.fulcro.mutations :as m :refer [defmutation]])
+
+  (app/current-state app)
+  (comp/get-initial-state root/Root)
+  (comp/get-query root/Root)
+
+  (merge/merge-component!
+    app root/Root {:index/id 2
+                   :index/text "Foobar"})
+
+  (dr/change-route app (dr/path-to root/Index))
+  (dr/change-route app ["index"])
+
+  (route-to! ["settings"])
+  (route-to! ["game"])
+  (route-to {:path (dr/path-to root/Settings)}))
--- src/main/beatthemarket/ui/root.cljs
+++ src/main/beatthemarket/ui/root.cljs
@@ -1,18 +1,103 @@
 (ns beatthemarket.ui.root
   (:require
-    [fulcro.client.dom :as dom :refer [div ul li p h3]]
-    [fulcro.client.primitives :as prim :refer [defsc]]
-    [beatthemarket.model.user :as user]
-    [beatthemarket.ui.components :as comp]
-    [beatthemarket.ui.stock-chart :as stock-chart]
-    [taoensso.timbre :as log]))
+   [fulcro.client.dom :as dom :refer [div ul li p h3]]
+   [fulcro.client.primitives :as prim]
+   [com.fulcrologic.fulcro.routing.dynamic-routing :as dr]
+   [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
+
+   [beatthemarket.model.user :as user]
+   [beatthemarket.ui.stock-chart :as stock-chart]
+   [taoensso.timbre :as log]))
 
 
-(defsc Root [this _]
-  {:initial-state (fn [_]
-                    {:stock-chart (prim/get-initial-state stock-chart/StockChart {:label "StockChart"})})
+(defsc Index [this {:index/keys [id text]}]
+  {:query [:index/id :index/text]
+   :ident (fn [] [:component/id :index])
+   :route-segment ["index"]
+   :initial-state {:index/id 1
+                   :index/text :param/text}}
 
-   :componentDidMount (fn [_] (log/info (prim/get-initial-state Root {})))}
+  (h3 text))
 
-  (div
-    (stock-chart/ui-stock-chart {})))
+(def ui-index (comp/factory Index))
+
+(defsc Landing [this {:landing/keys [id text]}]
+  {:query [:landing/id :landing/text]
+   :ident (fn [] [:component/id :landing])
+   :route-segment ["landing"]
+   :initial-state {:landing/id 1
+                   :landing/text :param/text}}
+
+  (h3 text))
+
+(def ui-landing (comp/factory Landing))
+
+(defsc Settings [this {:settings/keys [id text]}]
+  {:query [:settings/id :settings/text]
+   :ident (fn [] [:component/id :settings])
+   :route-segment ["settings"]
+   :initial-state {:settings/id 1
+                   :settings/text :param/text}}
+
+  (h3 text))
+
+(def ui-settings (comp/factory Settings))
+
+(defsc Game [this {:game/keys [id text]}]
+  {:query [:game/id :game/text]
+   :ident (fn [] [:component/id :game])
+   :route-segment ["game"]
+   :initial-state {:game/id 1
+                   :game/text :param/text}}
+
+  (h3 text))
+
+(def ui-game (comp/factory Game))
+
+
+(dr/defrouter TopRouter [this {:keys [current-state] :as props}]
+  {:router-targets [Index Landing Game Settings]}
+
+  (log/info "TopRouter Props /" props)
+  (case current-state
+    :pending (dom/div "Loading...")
+    :failed (dom/div "Failed!")
+
+    (dom/div "No route selected.")))
+
+(def ui-top-router (comp/factory TopRouter))
+
+
+;; (defsc Root [this {:root/keys [index landing game settings] :as props}]
+;;   {:query [{:root/index (comp/get-query Index)}
+;;            {:root/landing (comp/get-query Landing)}
+;;            {:root/game (comp/get-query Game)}
+;;            {:root/settings (comp/get-query Settings)}]
+;;
+;;    :initial-state {:root/index {:text "Index Text"}
+;;                    :root/landing {:text "Landing Text"}
+;;                    :root/game {:text "Game Text"}
+;;                    :root/settings {:text "Settings Text"}}}
+;;
+;;   :query [{:root/router (comp/get-query TopRouter)}]
+;;
+;;
+;;   (log/info "Root Props /" this props)
+;;   (when index
+;;     (dom/div (ui-index index))))
+
+(defsc Root [this {:root/keys [router index landing game settings]}]
+  {:query [{:root/router (comp/get-query TopRouter)}
+           {:root/index (comp/get-query Index)}
+           {:root/landing (comp/get-query Landing)}
+           {:root/game (comp/get-query Game)}
+           {:root/settings (comp/get-query Settings)}]
+
+   :initial-state {:root/router {}
+                   :root/index {:text "Index Text"}
+                   :root/landing {:text "Landing Text"}
+                   :root/game {:text "Game Text"}
+                   :root/settings {:text "Settings Text"}}}
+
+  (when router
+    (dom/div (ui-top-router router))))
