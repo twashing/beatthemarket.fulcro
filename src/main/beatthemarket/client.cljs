9af678aed2d22cad2bc346db0fbf2a6a39a0281b
Timothy Washington <twashing@interruptsoftware.com>
2020-04-26 14:44:22 -0400

    First pass at adding HTML5 History and 2-way binding between browser bar and app state

--- src/main/beatthemarket/client.cljs
+++ src/main/beatthemarket/client.cljs
@@ -1,5 +1,8 @@
 (ns beatthemarket.client
-  (:require [taoensso.timbre :as log]
+  (:require [clojure.string :as str]
+            [taoensso.timbre :as log]
+            [pushy.core :as pushy]
+            [edn-query-language.core :as eql]
             [fulcro.client :as fc]
 
             [fulcro.client.network :as net]
@@ -29,6 +32,47 @@
 (defn ^:export connect-websocket []
   (comp/transact! app `[(connect-socket {})]))
 
+
+;; ====>
+
+(defn url->path
+  "Given a url of the form \"/gift/123/edit?code=abcdef\", returns a
+  path vector of the form [\"gift\" \"123\" \"edit\"]. Assumes the url
+  starts with a forward slash. An empty url yields the path [\"home\"]
+  instead of []."
+  [url]
+  (-> url (str/split "?") first (str/split "/") rest vec))
+
+(defn path->url
+  "Given a path vector of the form [\"gift\" \"123\" \"edit\"],
+  returns a url of the form \"/gift/123/edit\"."
+  [path]
+  (str/join (interleave (repeat "/") path)))
+
+(defn routable-path?
+  "True if there exists a router target for the given path."
+  [app path]
+  (let [state-map  (app/current-state app)
+        root-class (app/root-class @app)
+        root-query (comp/get-query root-class state-map)
+        ast        (eql/query->ast root-query)]
+    (some? (dr/ast-node-for-route ast path))))
+
+(def default-route ["index"])
+
+(defonce history (pushy/pushy
+                   (fn [path]
+                     (log/info "pushy/pushy A /" path)
+                     (dr/change-route app path))
+                   (fn [url]
+                     (log/info "pushy/pushy B /" url)
+                     (let [path (url->path url)]
+                       (log/info "pushy/pushy B.i /" path)
+                       (if (routable-path? app path)
+                         path
+                         default-route)))))
+
+;; ====>
 
 (defn get-chart-series []
   (first
@@ -65,8 +109,22 @@
                                }))
   (app/mount! @app root/Root "app" {:initialize-state? true})
   (dr/initialize! @app)
+  (pushy/start! history)
   (dr/change-route app (dr/path-to root/Index)))
 
+(defn route-to! [path]
+
+  ;; (log/info "route-to! /" path)
+  (pushy/set-token! history (path->url path)))
+
+;; TODO On initial load, use URI route
+;; TODO Fix route-to mutation
+(defmutation route-to
+  "Mutation to go to a specific route"
+  [{:keys [path]}]
+  (action [_]
+          (log/info "route-to / path /" path)
+          (route-to! path)))
 
 (comment
 
@@ -93,4 +151,8 @@
                    :index/text "Foobar"})
 
   (dr/change-route app (dr/path-to root/Index))
-  (dr/change-route app ["index"]))
+  (dr/change-route app ["index"])
+
+  (route-to! ["settings"])
+  (route-to! ["game"])
+  (route-to {:path (dr/path-to root/Settings)}))
